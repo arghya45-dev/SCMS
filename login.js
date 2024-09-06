6d65c59a39343909c24c641c9bbbe3143e007949
arghya45-dev <chakrabortyarghya11@gmail.com>
2024-09-06 11:05:36 +0530

    Add log in page

---
+++ login.js
@@ -0,0 +1,14 @@
+const loginBtn = document.getElementById('loginBtn');
+const signupBtn = document.getElementById('signupBtn');
+const loginForm = document.getElementById('loginForm');
+const signupForm = document.getElementById('signupForm');
+
+loginBtn.addEventListener('click', () => {
+  loginForm.classList.add('active');
+  signupForm.classList.remove('active');
+});
+
+signupBtn.addEventListener('click', () => {
+  signupForm.classList.add('active');
+  loginForm.classList.remove('active');
+});
